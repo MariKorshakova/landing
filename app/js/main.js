6a0d403d55e51db49967165f337fc934197c2b79
MariKorshakova <mari.korshakova@gmail.com>
2023-04-26 18:02:14 +0300

    all commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,23 @@
 $(function(){
+
+   $('.traking-slider__items').slick({
+		arrows:false,
+		dots: true,
+	});
+
+	$('.header-btn').on('click', function () {
+		$('.header-btn').removeClass('header-btn--active');
+		$(this).addClass('header-btn--active');
+	});
 	
+	$('.select-style').styler();
 });
+
+function myFunction() {
+	var x = document.getElementById("myLinks");
+	if (x.style.display === "none") {
+	  x.style.display = "block";
+	} else {
+	  x.style.display = "none";
+	}
+ }
